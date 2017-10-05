1917d253fc3fd0be0e73060df0d8c109730031f0
Dasari, Kiran <Kiran.Dasari@Point72.com>
2017-10-05 04:43:27 -0400

    initial commit

---
+++ src/main/java/WithGeneratedCode.java
@@ -0,0 +1,52 @@
+import example.avro.User;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+import java.io.IOException;
+
+public class WithGeneratedCode {
+    public static void main(String[] args) throws IOException {
+        User user1 = new User();
+        user1.setName("Alyssa");
+        user1.setFavoriteNumber(256);
+        // Leave favorite color null
+
+        // Alternate constructor
+        User user2 = new User("Ben", 7, "red");
+
+        // Construct via builder
+        User user3 = User.newBuilder()
+                .setName("Charlie")
+                .setFavoriteColor("blue")
+                .setFavoriteNumber(null)
+                .build();
+
+        // Serialize user1, user2 and user3 to disk
+        File file = new File("users.avro");
+
+        DatumWriter<User> userDatumWriter = new SpecificDatumWriter(User.class);
+        DataFileWriter<User> dataFileWriter = new DataFileWriter(userDatumWriter);
+        dataFileWriter.create(user1.getSchema(), file);
+        dataFileWriter.append(user1);
+        dataFileWriter.append(user2);
+        dataFileWriter.append(user3);
+        dataFileWriter.close();
+
+        // Deserialize Users from disk
+        DatumReader<User> userDatumReader = new SpecificDatumReader<User>(User.class);
+        DataFileReader<User> dataFileReader = new DataFileReader(file, userDatumReader);
+        User user = null;
+        while (dataFileReader.hasNext()) {
+            // Reuse user object by passing it to next(). This saves us from
+            // allocating and garbage collecting many objects for files with
+            // many items.
+            user = dataFileReader.next(user);
+            System.out.println(user);
+        }
+    }
+}
